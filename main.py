b2bad4708a54e5f16ed9907b6fad4b2d0a175886
bqio <mihalzov.dima@yandex.ru>
2021-10-15 13:23:24 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,54 @@
+from PIL import Image, ImageDraw
+
+PADDING = 30
+RGBA_RED = (255, 0, 0, 255)
+RGBA_BLACK = (0, 0, 0, 255)
+RGBA_LIMIT = (40, 40, 40, 255)
+RGBA_TRANSPARENT = (0, 0, 0, 0)
+IMAGE_PATH = "C:\\Users\\admin\\Desktop\\eye_pat.jpg"
+
+
+def main():
+    # Открываем изображение
+    image = Image.open(IMAGE_PATH)
+
+    # Преобразовываем изображение в формат RGBA
+    image = image.convert("RGBA")
+
+    # Определяем рамку с отступом в 25 пикселей
+    box = (PADDING, PADDING, image.width - PADDING, image.height - PADDING)
+
+    # Обрезаем изображение по рамке
+    image = image.crop(box)
+
+    # Создаем маску для удаления лишнего черного цвета за областью глаза
+    mask = Image.new("L", image.size, 0)
+    draw = ImageDraw.Draw(mask)
+    draw.ellipse((0, 0, image.size[0], image.size[1]), fill=255)
+    image.putalpha(mask)
+
+    # Получаем массив пикселей
+    pixels = list(image.getdata())
+
+    # Считаем количество прозрачных пикселей и пикселей с патологией
+    transparent_pixel_count = 0
+    black_pixel_count = 0
+    for i in range(image.height):
+        for j in range(image.width):
+            pixel = image.getpixel((i, j))
+            if pixel == RGBA_TRANSPARENT:
+                transparent_pixel_count += 1
+            if pixel <= RGBA_LIMIT and pixel != RGBA_TRANSPARENT:
+                image.putpixel((i, j), RGBA_RED)
+                black_pixel_count += 1
+
+    # Высчитываем процент пикселей с патологией от общего количества пикселей
+    total_pixels = len(pixels) - transparent_pixel_count
+    percent = round((black_pixel_count / total_pixels) * 100)
+
+    print("Total pathology: {}%".format(percent))
+    image.show()
+
+
+if __name__ == "__main__":
+    main()
