42a68cd57610c1a817fd9889ba3d1345f305fb6b
bqio <mihalzov.dima@yandex.ru>
2021-10-21 13:35:42 +0300

    fix bugs

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ RGBA_RED = (255, 0, 0, 255)
 RGBA_BLACK = (0, 0, 0, 255)
 RGBA_LIMIT = (40, 40, 40, 255)
 RGBA_TRANSPARENT = (0, 0, 0, 0)
-IMAGE_PATH = "C:\\Users\\admin\\Desktop\\eye_pat.jpg"
+IMAGE_PATH = "C:\\Users\\admin\\Desktop\\eye.jpg"
 
 
 def main():
@@ -15,7 +15,7 @@ def main():
     # Преобразовываем изображение в формат RGBA
     image = image.convert("RGBA")
 
-    # Определяем рамку с отступом в 25 пикселей
+    # Определяем рамку с отступом в 30 пикселей
     box = (PADDING, PADDING, image.width - PADDING, image.height - PADDING)
 
     # Обрезаем изображение по рамке
@@ -27,23 +27,20 @@ def main():
     draw.ellipse((0, 0, image.size[0], image.size[1]), fill=255)
     image.putalpha(mask)
 
-    # Получаем массив пикселей
-    pixels = list(image.getdata())
-
     # Считаем количество прозрачных пикселей и пикселей с патологией
     transparent_pixel_count = 0
     black_pixel_count = 0
     for i in range(image.height):
         for j in range(image.width):
-            pixel = image.getpixel((i, j))
+            pixel = image.getpixel((j, i))
             if pixel == RGBA_TRANSPARENT:
                 transparent_pixel_count += 1
             if pixel <= RGBA_LIMIT and pixel != RGBA_TRANSPARENT:
-                image.putpixel((i, j), RGBA_RED)
+                image.putpixel((j, i), RGBA_RED)
                 black_pixel_count += 1
 
     # Высчитываем процент пикселей с патологией от общего количества пикселей
-    total_pixels = len(pixels) - transparent_pixel_count
+    total_pixels = (image.height * image.width) - transparent_pixel_count
     percent = round((black_pixel_count / total_pixels) * 100)
 
     print("Total pathology: {}%".format(percent))
